086fbf6cd0a1251ccb5d16ef0378b361b8094010
iamadim <adim4usall@gmail.com>
2020-04-29 02:06:41 -0400

    Updates

---
+++ precache-manifest.32ae60908d61233b25a016348d28028b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dba08f8bc1b8533721f6fc572beb9e3",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "7c62d37ebe058b68ab51",
+    "url": "/food-recipe/static/css/main.a0c6946e.chunk.css"
+  },
+  {
+    "revision": "c68ad9660c9915e81709",
+    "url": "/food-recipe/static/js/2.6f99de36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.6f99de36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c62d37ebe058b68ab51",
+    "url": "/food-recipe/static/js/main.c7a729be.chunk.js"
+  },
+  {
+    "revision": "152afc57554b959fac4d",
+    "url": "/food-recipe/static/js/runtime-main.5197e6fa.js"
+  }
+]);
